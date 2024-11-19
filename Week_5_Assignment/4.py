47f2e317e2d8868ce4ca7906acaebd56083e3e7e
Subhajit Manna <113898471+SubhajitManna-B@users.noreply.github.com>
2024-11-19 21:39:54 +0530

    Add files via upload
    Assignment week 5,6,7

---
+++ Week_5_Assignment/4.py
@@ -0,0 +1,22 @@
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+# I. Find the length of the set it_companies
+print("Length of it_companies:", len(it_companies))
+
+# II. Add 'Twitter' to it_companies
+it_companies.add('Twitter')
+print("It_companies after adding 'Twitter':", it_companies)
+
+# III. Insert multiple IT companies at once to the set it_companies
+new_companies = set(input("Enter IT companies to add (separated by commas): ").split(','))
+it_companies.update(new_companies)
+print("It_companies after adding new companies:", it_companies)
+
+# IV. Remove one of the companies from the set it_companies
+company_to_remove = input("Enter the IT company to remove: ")
+it_companies.remove(company_to_remove) if company_to_remove in it_companies else print(f"{company_to_remove} not found in the set")
+print("It_companies after removal:", it_companies)
+
